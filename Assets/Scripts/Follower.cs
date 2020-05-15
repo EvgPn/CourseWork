8aa421dd3822b9307d9b486d3967884e04156c8b
Evg Pn <ye.ponomarenko@gmail.com>
2020-05-15 16:00:15 +0300

    feat(project)
    -create project
    -add path creator tool
    -add models of roundabout and cars
    -create paths for roundabout movement

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Follower : MonoBehaviour
+{
+	public PathCreator pathCreator;
+	public float moveSpeed = 30f;
+	private float distanceTravelled;
+
+	private void Update()
+	{
+		distanceTravelled += moveSpeed * Time.deltaTime;
+		transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+		transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+	}
+}
